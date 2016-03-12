4c464bc76779cb3787be7166c36d8c316a720977
Xi Zhang <xizhangmit@gmail.com>
2016-03-12 16:20:38 -0500

    remove tsd typings from git

--- src/typings/tsd.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="angularjs/angular.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="express/express.d.ts" />
-/// <reference path="mime/mime.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="serve-static/serve-static.d.ts" />
-/// <reference path="body-parser/body-parser.d.ts" />
